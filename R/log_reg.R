d567e4b496125f2a9a98e9fb424c6bf5be063c0c
jemilner <37807444+jemilner@users.noreply.github.com>
2022-05-21 11:59:58 +0100

    regulated log reg

--- R/log_reg.R
+++ R/log_reg.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(mice)
+library(glmnet)
 
 set.seed(1)
 
@@ -23,22 +24,83 @@ my_tbl <- bind_rows(
 
 #set training indices
 train <- sample(c(TRUE, FALSE), size = nrow(my_tbl), replace = T, prob = c(0.8, 0.2))
+#true values
+true_rating <- my_tbl %>%
+    slice((1:nrow(my_tbl))[!train]) %>%
+    pull(OUTSTANDING)
 
-#not including pnorg
-outstanding_fit <- glm(OUTSTANDING ~ ISPOST16 + NOR + PSENELSE + PSENELK + #SCHOOLTYPE +
-                           PNUMENGFL + PNUMFSMEVER + PUPILTEACHERRATIO + PUPILTARATIO + 
-                           PUPILSUPPORTRATIO + MIXED + ADMPOL_SELECTIVE + RELCHAR_YES +
-                           MINORGROUP_ACADEMY + PUPILTARATIOLOG + PUPILSUPPORTRATIOLOG,
-                       data = my_tbl,
-                       family = binomial,
-                       subset = train)
-outstanding_probs <- predict(outstanding_fit,
-                             newdata = my_tbl[!train,],
-                             type = "response")
+###################
+## basic log reg ##
+###################
+basic_fit <- glm(OUTSTANDING ~ ISPOST16 + NOR + PSENELSE + PSENELK + #SCHOOLTYPE +
+                     PNUMENGFL + PNUMFSMEVER + PUPILTEACHERRATIO + PUPILTARATIO + 
+                     PUPILSUPPORTRATIO + MIXED + ADMPOL_SELECTIVE + RELCHAR_YES +
+                     MINORGROUP_ACADEMY + PUPILTARATIOLOG + PUPILSUPPORTRATIOLOG,
+                 data = my_tbl,
+                 family = binomial,
+                 subset = train)
+basic_probs <- predict(outstanding_fit,
+                       newdata = my_tbl[!train,],
+                       type = "response")
 
-pred_rating <- ifelse(outstanding_probs > 0.5, 1, 0)
+basic_rating <- ifelse(basic_probs > 0.5, 1, 0)
 
-true_rating = my_tbl$OUTSTANDING[!train]
-table(pred_rating, true_rating)
+table(basic_rating, true_rating)
+mean(basic_rating == true_rating)
 
-mean(pred_rating == true_rating)
+#######################
+## regulated log reg ##
+#######################
+reg_cov <- model.matrix(OUTSTANDING ~ ISPOST16 + NOR + PSENELSE + PSENELK + #SCHOOLTYPE +
+                            PNUMENGFL + PNUMFSMEVER + PUPILTEACHERRATIO + PUPILTARATIO + 
+                            PUPILSUPPORTRATIO + MIXED + ADMPOL_SELECTIVE + RELCHAR_YES +
+                            MINORGROUP_ACADEMY + PUPILTARATIOLOG + PUPILSUPPORTRATIOLOG,
+                        data = my_tbl)[,-1]
+
+reg_outcome <- my_tbl %>% pull(OUTSTANDING)
+
+#0 = full ridge, 1 = full lasso
+reg_alpha <- 0.9
+
+#obtain best lambda (reg penalty term) estimate in terms of MSE using cross validation
+lambda_cv <- cv.glmnet(
+    x = reg_cov,
+    y = reg_outcome,
+    alpha = reg_alpha
+)
+
+plot(lambda_cv)
+plot(lambda_cv$glmnet.fit, "lambda")
+
+lambda_min <- lambda_cv$lambda.min
+lambda_1se <- lambda_cv$lambda.1se
+
+reg_model <- glmnet(
+    x = reg_cov,
+    y = reg_outcome,
+    alpha = reg_alpha, 
+    lambda = lambda_min
+)
+
+#compare basic and reg coeffs
+signif(
+    cbind(
+        basic = basic_fit$coefficients,
+        reg = c(reg_model$a0,  as.vector(reg_model$beta))
+    ),
+    3
+)
+
+reg_probs <- predict.glmnet(
+    reg_model,
+    newx = my_tbl %>%
+        select(-c(URN, SCHOOLTYPE, OUTSTANDING)) %>%
+        slice((1:nrow(my_tbl))[!train])%>%
+        as.matrix(),
+    type = "response"
+)
+
+reg_rating <- ifelse(reg_probs > 0.5, 1, 0)
+
+table(reg_rating, true_rating)
+mean(reg_rating == true_rating)
